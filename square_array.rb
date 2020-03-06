b9611daadffd75810ca97349f512a5b07c4b2cd0
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-03-06 17:53:18 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,6 @@
 def square_array(array)
   numbers = ["2", "4", "6"]
   numbers.each do |numbers|
-    if numbers = numbers**2
-      puts numbers
-    else
-      numbers
-    end
-  end
+    numbers **= 2
+    print numbers
 end
