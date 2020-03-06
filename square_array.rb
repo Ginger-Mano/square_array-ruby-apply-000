c0d320b853779420e409d61bd424b3f879ec806b
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-03-06 21:50:02 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,6 +3,6 @@ numbers = [1, 2, 3]
 def square_array(numbers)
   array = [1, 4, 9]
 array.each do |numbers|
-    puts ["#{numbers[0]}**2, #{numbers[1]}**2, #{numbers[2]}**2"]
+    puts ([1, 2, 3]**2)
   end
 end
